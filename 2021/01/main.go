0704bb66f6f4e3bb660d1c1815d42b5fa7a80cfa
Sabin <sanislav@gmail.com>
2021-12-01 08:36:04 +0200

    Simplify day 01

--- 2021/01/main.go
+++ 2021/01/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"sort"
 	"strconv"
 	"strings"
 )
@@ -13,52 +12,24 @@ func main() {
 	lines := strings.Split(strings.TrimSpace(string(input)), "\n")
 	ansP1 := 0
 	ansP2 := 0
-	beforeP1 := 0
-	beforeP2 := 0
-	groups := map[int]int{}
-	keys := make([]int, 0)
 
-	for i, l := range lines {
-		mesasurement, _ := strconv.Atoi(l)
-
-		if (mesasurement == 0) { // last line
-			break
-		}
-
-		if (i + 2 < len(lines)) {
-			mesasurement2, _ := strconv.Atoi(lines[i+1])
-			mesasurement3, _ := strconv.Atoi(lines[i+2])
-
-			groups[i] = mesasurement + mesasurement2 + mesasurement3
-			keys = append(keys, i)
-		}
-
-		if beforeP1 == 0 {
-			beforeP1 = mesasurement
+	for i := range lines {
+		if (i == 0) {
 			continue
 		}
+		current, _ := strconv.Atoi(lines[i])
+		previous, _ := strconv.Atoi(lines[i-1])
 
-		if mesasurement > beforeP1 {
+		if (current > previous) {
 			ansP1++;
 		}
 
-		beforeP1 = mesasurement
-	}
-
-	sort.Ints(keys)
-
-	for _, k := range keys {
-		if beforeP2 == 0 {
-			beforeP2 = groups[k]
-			continue
+		if (i >= 3) {
+			previous3, _ := strconv.Atoi(lines[i-3])
+			if (current > previous3) {
+				ansP2++;
+			}
 		}
-
-		if groups[k] > beforeP2 {
-			ansP2++;
-		}
-
-		beforeP2 = groups[k]
-
 	}
 
 	fmt.Println(ansP1)
