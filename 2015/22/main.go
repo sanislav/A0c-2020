72aeecfdac635f5aab9cd4a4e0f72ee9248dd7ac
Sabin <sanislav@gmail.com>
2021-12-05 14:32:00 +0200

    Cleanup day 05 code

--- 2015/22/main.go
+++ 2015/22/main.go
@@ -1,30 +1,130 @@
 package main
 
-import (
-	"fmt"
-	"io/ioutil"
-	"strings"
-	// "strconv"
-	// "sort"
-)
+import "fmt"
+
+// "strconv"
+// "sort"
 
 // Magic Missile costs 53 mana. It instantly does 4 damage.
 // Drain costs 73 mana. It instantly does 2 damage and heals you for 2 hit points.
 // Shield costs 113 mana. It starts an effect that lasts for 6 turns. While it is active, your armor is increased by 7.
 // Poison costs 173 mana. It starts an effect that lasts for 6 turns. At the start of each turn while it is active, it deals the boss 3 damage.
 // Recharge costs 229 mana. It starts an effect that lasts for 5 turns. At the start of each turn while it is active, it gives you 101 new mana.
+type boss struct {
+	hp int
+	damage int
+}
 
+type player struct {
+	hp int
+	mana int
+}
+
+type abbilityProperties struct {
+	name string
+	manaCost int
+	manaGain int
+	damage int
+	healing int
+	increaseArmour int
+	turnsActive int
+}
 
 func main() {
-	input, _ := ioutil.ReadFile("input.txt")
-	lines := strings.Split(strings.TrimSpace(string(input)), "\n")
-	ansP1 := 0
-	ansP2 := 0
+	playerWin := false
+	bossDamage := 9
 
-	for _, s := range lines {
-		fmt.Println(s)
+	for (!playerWin) {
+		boss := boss{51, bossDamage}
+		player := player{50, 500}
+
+		playerOptions := []abbilityProperties{}
+
+		playerOptions = append(playerOptions, abbilityProperties{name: "mm", manaCost: 53, damage: 4})
+		playerOptions = append(playerOptions, abbilityProperties{name: "drain", manaCost: 73, damage: 2, healing: 2})
+		playerOptions = append(playerOptions, abbilityProperties{name: "shield", manaCost: 113, increaseArmour: 2, turnsActive: 6})
+		playerOptions = append(playerOptions, abbilityProperties{name: "poison", manaCost: 173, damage: 3, turnsActive: 6})
+		playerOptions = append(playerOptions, abbilityProperties{name: "recharge", manaCost: 229, manaGain: 101, turnsActive: 5})
+
+		tickers := []abbilityProperties{}
+		nextTickers := []abbilityProperties{}
+		solutions := map[string]bool{}
+
+		solutionHash := ""
+
+		for player.hp > 0 && boss.hp > 0 {
+			// ticker effects
+			for _, tick := range(tickers) {
+				boss, player, tick = applySpell(boss, player, tick)
+	
+				if tick.turnsActive > 0 {
+					nextTickers = append(nextTickers, tick)
+				}
+			}
+
+			tickers = nextTickers
+
+			// choose next ability
+			castIndex := -1
+			for i, ability := range(playerOptions) {
+				if player.mana >= ability.manaCost &&  {
+					castIndex = i
+					break
+				}
+			}
+
+			solutionHash += playerOptions[castIndex].name
+			fmt.Println("Player casts: " + playerOptions[castIndex].name)
+
+			if (castIndex >= 0) {
+				player.hp = 0
+				solutions[solutionHash] = false
+			}
+
+			if val, ok := solutions[solutionHash]; ok {
+				if (val == false) {
+					break
+				}
+			}
+
+			if (playerOptions[castIndex].turnsActive == 0) {
+				boss, player, _ = applySpell(boss, player, playerOptions[castIndex])
+			}
+
+			player.mana -= playerOptions[castIndex].manaCost
+			player.hp -= boss.damage
+			boss.damage = bossDamage
+
+			if (boss.hp <= 0) {
+				playerWin = true
+
+				fmt.Println(player)
+				fmt.Println(boss)
+
+			}
+			solutions[solutionHash] = true
+
+			if (player.hp <= 0) {
+				solutions[solutionHash] = false
+			}
+		}
+
+		fmt.Println(solutions)
+
+	}
+}
+
+// apply effects of spell on boss and player
+func applySpell(boss boss, player player, spell abbilityProperties) (boss, player, abbilityProperties) {
+	boss.hp -= spell.damage
+	boss.damage -= spell.increaseArmour
+
+	player.mana += spell.manaGain
+	player.hp += spell.healing
+
+	if (spell.turnsActive > 0) {
+		spell.turnsActive--;
 	}
 
-	fmt.Println(ansP1)
-	fmt.Println(ansP2)
+	return boss, player, spell
 }
--- 2021/05/main.go
+++ 2021/05/main.go
@@ -8,36 +8,34 @@ import (
 	"strings"
 )
 
+type Matrix struct {
+	data [1000][1000]int
+}
+
 func main() {
 	input, _ := ioutil.ReadFile("input.txt")
 	lines := strings.Split(strings.TrimSpace(string(input)), "\n")
-	ansP1 := 0
-	ansP2 := 0
 
-	m := getLines(lines, false)
-	m2 := getLines(lines, true)
-	for i := 0; i < len(m[0]); i++ {
-		for j := 0; j < len(m[i]); j++ {
-			if m[i][j] > 1 {
-				ansP1++
-			}
-		}
-	}
-
-	for i := 0; i < len(m2[0]); i++ {
-		for j := 0; j < len(m2[i]); j++ {
-			if m2[i][j] > 1 {
-				ansP2++
-			}
-		}
-	}
-
-	fmt.Println(ansP1)
-	fmt.Println(ansP2)
+	calculateAnswer(generateMatrix(lines, false))
+	calculateAnswer(generateMatrix(lines, true))
 }
 
-func getLines(lines []string, p2 bool) [1000][1000]int {
-	m := [1000][1000]int{}
+func calculateAnswer(m Matrix) {
+	ans := 0
+
+	for i := 0; i < len(m.data[0]); i++ {
+		for j := 0; j < len(m.data[i]); j++ {
+			if m.data[i][j] > 1 {
+				ans++
+			}
+		}
+	}
+
+	fmt.Println(ans)
+}
+
+func generateMatrix(lines []string, p2 bool) Matrix {
+	m := Matrix{}
 
 	for _, line := range(lines) {
 		parts := strings.Split(line, " -> ")
@@ -56,20 +54,20 @@ func getLines(lines []string, p2 bool) [1000][1000]int {
 		if (sameLine) {
 			if x1 < x2 {
 				for i := x1; i <= x2; i ++ {
-					m[y1][i] += 1
+					m.data[y1][i] += 1
 				}
 			} else if x2 < x1 {
 				for i := x2; i <= x1; i ++ {
-					m[y1][i] += 1
+					m.data[y1][i] += 1
 				}
 			}
 			if y1 < y2 {
 				for i := y1; i <= y2; i ++ {
-					m[i][x1] += 1
+					m.data[i][x1] += 1
 				}
 			} else if y2 < y1 {
 				for i := y2; i <= y1; i ++ {
-					m[i][x1] += 1
+					m.data[i][x1] += 1
 				}
 			}
 		}
@@ -79,9 +77,9 @@ func getLines(lines []string, p2 bool) [1000][1000]int {
 				c := 0
 				for i := x1; i <= x2; i ++ {
 					if (y1 < y2) {
-						m[y1 + c][i] += 1
+						m.data[y1 + c][i] += 1
 					} else {
-						m[y1 - c][i] += 1
+						m.data[y1 - c][i] += 1
 					}
 					c++
 				}
@@ -89,9 +87,9 @@ func getLines(lines []string, p2 bool) [1000][1000]int {
 				c := 0
 				for i := x1; i >= x2; i -- {
 					if (y1 < y2) {
-						m[y1 + c][i] += 1
+						m.data[y1 + c][i] += 1
 					} else {
-						m[y1 - c][i] += 1
+						m.data[y1 - c][i] += 1
 					}
 					c++
 				}
