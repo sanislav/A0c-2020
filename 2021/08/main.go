238becaf7327a9c7bd6ed4b859d3ff5bf9b6e423
Sabin <sanislav@gmail.com>
2021-12-09 18:30:48 +0200

    Day 09 solutuion 2021

--- 2021/08/main.go
+++ 2021/08/main.go
@@ -6,30 +6,28 @@ import (
 	"strings"
 )
 
-type Matrix struct {
-	data [1000][1000]int
-}
-
 func main() {
 	input, _ := ioutil.ReadFile("input.txt")
 	lines := strings.Split(strings.TrimSpace(string(input)), "\n")
 
 	ansP1 := 0
+	ansP2 := 0
 
 	for _, line := range(lines) {
 		// fmt.Println(line)
 
 		parts := strings.Split(line, " | ")
 
-		// siglans := strings.Split(parts[0], " ")
+		signals := strings.Split(parts[0], " ")
 		output := strings.Split(parts[1], " ")
 
 		for _, d := range(output) {
 			// 1 4, 7, 8
-
 			if len(d) == 2 || len(d) == 4 || len(d) == 3 || len(d) == 7 {
 				ansP1 += 1
 			}
+
+			ansP2 += calculateLineSum(signals, output)
 		}
 	}
 
@@ -37,4 +35,44 @@ func main() {
 
 
 	fmt.Println(ansP1)
+	fmt.Println(ansP2)
+}
+
+
+func calculateLineSum(signals []string, output []string) int {
+	m := map[int]map[string]bool{}
+
+	for _, signal := range(signals) {
+		digit := map[string]bool{}
+
+		if len(signal) == 2 {
+			digit["c"] = true;
+			digit["f"] = true;
+
+			m[1] = digit;
+		}
+
+		if len(signal) == 3 {
+			digit["a"] = true;
+			digit["c"] = true;
+			digit["f"] = true;
+
+			m[1] = digit;
+		}
+
+		if len(signal) == 2 {
+			digit["c"] = true;
+			digit["f"] = true;
+
+			m[1] = digit;
+		}
+
+		if len(signal) == 2 {
+			digit["c"] = true;
+			digit["f"] = true;
+
+			m[1] = digit;
+		}
+	}
+	return 0;
 }
---
+++ 2021/09/main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+ const ml = 102
+ const mc = 102
+
+// const ml = 7
+// const mc = 12
+
+var basins = map[string]map[string]bool{}
+var visited = map[string]bool{}
+var toVisit = [][]int{}
+
+func main() {
+	input, _ := ioutil.ReadFile("input.txt")
+	lines := strings.Split(strings.TrimSpace(string(input)), "\n")
+
+	ansP1 := 0
+
+	floor := [ml][mc]int{}
+
+	for i := 0; i < len(floor); i++ {
+		for j:= 0; j < len(floor[0]); j++ {
+			if i == 0 || j == 0 || i == len(floor) - 1 || j == len(floor[0]) - 1 {
+				floor[i][j] = math.MaxInt16
+			} else {
+				c := lines[i-1][j-1]
+				intC, _ := strconv.Atoi(string(c))
+				floor[i][j] = intC
+			}
+		}
+	}
+
+	basinCounts := []int{}
+	for i := 1; i < len(floor) - 1; i++ {
+		for j:= 1; j < len(floor[0]) -1; j++ {
+
+			if isLowest(floor, i, j) {
+				ansP1 += floor[i][j] + 1;
+				toVisit = [][]int{}
+				c := countBasin(floor, i, j, strconv.Itoa(i) + "_" + strconv.Itoa(j))
+				basinCounts = append(basinCounts, c)
+				// fmt.Println("Basin " + strconv.Itoa(i)  + "_" +  strconv.Itoa(j) + " has count " + strconv.Itoa(c))
+			}
+		}
+	}
+
+	sort.Ints(basinCounts)
+
+	fmt.Println(ansP1)
+	fmt.Println(basinCounts[len(basinCounts) - 1] * basinCounts[len(basinCounts) - 2] * basinCounts[len(basinCounts) - 3])
+}
+
+
+func isLowest(floor [ml][mc]int, i int, j int) bool {
+	return floor[i][j] < floor[i-1][j] && floor[i][j] < floor[i+1][j] && floor[i][j] < floor[i][j-1] && floor[i][j] < floor[i][j+1]
+}
+
+
+func countBasin(floor [ml][mc]int, i int, j int, basin string) int {
+	coords := strconv.Itoa(i)  + "_" +  strconv.Itoa(j)
+
+	r := addToBasin(basin, coords)
+	if r {
+		visited[coords] = true
+	}
+
+	if floor[i-1][j] != math.MaxInt16 && floor[i-1][j] != 9 {
+		coords := strconv.Itoa(i-1)  + "_" +  strconv.Itoa(j)
+		addToVistit(coords, i-1, j)
+	}
+	if floor[i+1][j] != math.MaxInt16 && floor[i+1][j] != 9 {
+		coords := strconv.Itoa(i+1)  + "_" +  strconv.Itoa(j)
+		addToVistit(coords, i+1, j)
+
+	}
+	if floor[i][j-1] != math.MaxInt16 && floor[i][j-1] != 9 {
+		coords := strconv.Itoa(i)  + "_" +  strconv.Itoa(j-1)
+		addToVistit(coords, i, j-1)
+
+	}
+	if floor[i][j+1] != math.MaxInt16 && floor[i][j+1] != 9 {
+		coords := strconv.Itoa(i)  + "_" +  strconv.Itoa(j+1)
+		addToVistit(coords, i, j+1)
+
+	}
+
+	if (len(toVisit) == 0) {
+		return len(basins[basin])
+	}
+
+	// pop first el from toVisit and countBasin for it
+	next := toVisit[0]
+	toVisit = append(toVisit[:0], toVisit[1:]...)
+
+	return countBasin(floor, next[0], next[1], basin)
+}
+
+func addToVistit(coords string, i int, j int) {
+	if _, vis := visited[coords]; ! vis {
+		set := true
+		for _, c := range toVisit {
+			if c[0] == i && c[1] == j {
+				set = false
+				break
+			}
+		}
+		if (set) {
+			toVisit = append(toVisit, []int{i, j})
+		}
+	}
+}
+
+func addToBasin(basin string, coordsinates string) bool {
+	m := map[string]bool{}
+
+	if coords, ok := basins[basin]; !ok {
+		m[coordsinates] = true
+		basins[basin] = m
+		return true
+	} else {
+		if _, ok := coords[coordsinates]; !ok {
+			coords[coordsinates] = true
+			basins[basin] = coords
+			return true
+		}
+	}
+
+	return false
+}
